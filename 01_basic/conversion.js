bfb9a2caa94545c4bb1bca5f51a2b67f66ff5f3e
Aakash ojha <119150124+ghostcoder14@users.noreply.github.com>
2023-11-09 18:51:02 +0000

    conversion of datatypes

---
+++ 01_basic/conversion.js
@@ -0,0 +1,18 @@
+let age = 33 
+ let age1 = "33abc"
+console.log(typeof age)
+
+let valueinNumber = Number(age)
+console.log(typeof valueinNumber)
+console.log(valueinNumber)
+
+console.log(typeof age1)
+let valueInNumber = Number(age1)
+console.log(typeof  valueInNumber )
+console.log(valueInNumber)
+
+
+//  NaN => this is nnot a number
+// 33 => 33 
+// "33abc" => NaN
+// true => 1 ;  false => 0 
