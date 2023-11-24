31ff78866aafbe000b0ff7b667e580be0d012f83
Aakash ojha <119150124+ghostcoder14@users.noreply.github.com>
2023-11-24 17:46:30 +0000

    data type summary

---
+++ 01_basic/datatypr.js
@@ -0,0 +1,8 @@
+
+const score =  100 
+const scoreValue = 100.3 
+const isLoggedIn = false 
+let userEmail ;
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(Id === anotherId);
